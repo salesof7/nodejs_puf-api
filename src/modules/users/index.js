458900bcb7f49a4d0db42b498cfdb3736a0218bb
salesof7 <jpsaless2002@gmail.com>
2022-09-02 23:20:52 -0300

    feat: users route

---
+++ src/modules/users/index.js
@@ -0,0 +1,15 @@
+export const list = (ctx) => {
+  ctx.body = "Hello";
+};
+
+export const create = (ctx) => {
+  ctx.body = "Hello";
+};
+
+export const update = (ctx) => {
+  ctx.body = "Hello";
+};
+
+export const remove = (ctx) => {
+  ctx.body = "Hello";
+};
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Router from "@koa/router";
+import * as users from "./modules/users";
+
+export const router = new Router();
+
+router.get("/users", users.list);
+router.post("/users", users.create);
+router.put("/users/:id", users.update);
+router.delete("/users/:id", users.remove);
