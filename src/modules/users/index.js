8e4637f4b4b8f95b15454f330b25d7b6eb555d5f
salesof7 <jpsaless2002@gmail.com>
2022-09-05 16:09:35 -0300

    feat: users crud

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,15 +1,50 @@
-export const list = (ctx) => {
-  ctx.body = "Hello";
+import { prisma } from "../../data";
+
+export const list = async (ctx) => {
+  try {
+    const users = await prisma.user.findMany();
+    ctx.body = users;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = "Something went wrong, try again!";
+  }
 };
 
-export const create = (ctx) => {
-  ctx.body = "Hello";
+export const create = async (ctx) => {
+  try {
+    const user = await prisma.user.create({
+      data: ctx.request.body,
+    });
+    ctx.body = user;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = "Something went wrong, try again!";
+  }
 };
 
-export const update = (ctx) => {
-  ctx.body = "Hello";
+export const update = async (ctx) => {
+  const { name, email } = ctx.request.body;
+
+  try {
+    const user = await prisma.user.update({
+      where: { id: ctx.params.id },
+      data: { name, email },
+    });
+    ctx.body = user;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = "Something went wrong, try again!";
+  }
 };
 
-export const remove = (ctx) => {
-  ctx.body = "Hello";
+export const remove = async (ctx) => {
+  try {
+    await prisma.user.delete({
+      where: { id: ctx.params.id },
+    });
+    ctx.body = { id: ctx.params.id };
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = "Something went wrong, try again!";
+  }
 };
