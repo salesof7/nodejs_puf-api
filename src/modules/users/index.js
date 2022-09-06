54b074fcadb62fb1e7c7c798a539ae67afa46181
salesof7 <jpsaless2002@gmail.com>
2022-09-06 07:48:38 -0300

    feat: use basic auth strategy

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -3,8 +3,18 @@ import bcrypt from 'bcrypt';
 import { prisma } from '../../data';
 
 export const login = async (ctx) => {
+  const [type, credentials] = ctx.request.headers.authorization.split(' ');
+
+  if (type !== 'Basic') {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    const { email, password } = ctx.request.body;
+    const [email, password] = Buffer.from(credentials, 'base64')
+      .toString()
+      .split(':');
+
     const user = await prisma.user.findUnique({
       where: { email },
     });
